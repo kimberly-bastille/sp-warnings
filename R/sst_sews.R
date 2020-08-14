6a28b90a3dd319291f2108b0d669c328b568ef3b
kimberly-bastille <kimberly.bastille@gmail.com>
2020-08-14 08:50:22 -0400

    add csvs of data, and new plots to summary.rmd

--- R/sst_sews.R
+++ R/sst_sews.R
@@ -1,5 +1,7 @@
 library(raster)
 library(sf)
+library(tidyverse)
+library(dplyr)
 ### Needed functions
 na_aware_ews <- function(mat, cg_subsize) { 
   require(moments) # required packages
@@ -16,93 +18,11 @@ randomize_matrix_no_na <- function(mat) {
   return(mat)
 }
 
-## Example arguements
-epu_name <- "GB"
-month_number <- 12
-
-
-run_plot_sews<- function(epu_name, month_number){
-  ## set dirs
-  raw.dir<-here::here("data-raw/temp")
-  gis.dir<-here::here("data-raw/gis")
-  
-  # projection
-  crs <- "+proj=longlat +lat_1=35 +lat_2=45 +lat_0=40 +lon_0=-77+x_0=0+y_0=0 +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
-  
-  # Bring in epu polygons
-  epu <- rgdal::readOGR(file.path(gis.dir, "EPU_NOESTUARIES.shp"), verbose = T)
-  crs(epu) <- crs
-  
-  ### read in monthly mean oisst files 
-  fname<- list.files(raw.dir, pattern='*.nc',full.names=TRUE)
-  for (i in fname){
-    #r_stack <- raster::stack(i)
-    r_stack <- raster::stack(fname)
-    raster::crop(r_stack, extent(280,300,30,50))
-    r_stack <- raster::rotate(r_stack)
-    raster::crs(r_stack) <- crs
-    r_layer<-raster::unstack(r_stack) 
-    }
-  
-  ## Sort raster layers by month
-  #r_layer[[k]]@z[[1]] ## date defined here
-  monthlyRasters <- vector("list",12)
-  for (k in 1:length(r_layer)){
-    month <- lubridate::month(r_layer[[k]]@z[[1]])
-    monthlyRasters[[month]] <- c(monthlyRasters[[month]],r_layer[[k]])
-  }
-  
-  ## Select the month to be plotted
-  mon<-raster::stack(monthlyRasters[[month_number]])
-  
-  ### TEST CLOSE CROP SO AT LEAST IT WILL RUN
-  close_crop_gb <- as(extent(-71, -66, 40, 42), 'SpatialPolygons')
-  mon.test<-crop(mon, close_crop_gb)
-  mon.test<-unstack(mon.test)
-  ### MASK NOT WORKING 
-  ## Cut to EPU of choice
-   for (j in 1:length(mon)){
-     mon1<-raster::mask(mon, epu[epu$EPU == epu_name,])
-     mon1<-raster::unstack(mon1)
-   }
-  # for (j in 1:length(mon)){
-  #   mon1<-raster::mask(mon, epu[epu$EPU == epu_name,])
-  #   mon1<-raster::unstack(mon1)
-  #   }
-
-  ### FIX ME!!! ###
-  ## Run SEWS analysis
-  library(spatialwarningsGis)
-  indices <- spatialwarnings::compute_indicator(mon.test, fun = na_aware_ews,
-                                              cg_subsize = 2) ##### Erroring out "Error in check_mat(mat) : NAs in provided matrix."
-
-  indices_test <- spatialwarnings::indictest(indices,
-                                           nulln = 999,
-                                           null_method = randomize_matrix_no_na)
-
-  plot(indices_test)+ ggplot2::ggtitle(month_number, "oisst")
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #### Try on season.nc
 ## Example arguements
-epu_name <- "GB"
-season <- "fall"
+#epu_name <- "GB"
+#season <- "fall"
 
 
 gis.dir<-here::here("data-raw/gis")
@@ -116,7 +36,9 @@ epu <- rgdal::readOGR(file.path(gis.dir, "EPU_NOESTUARIES.shp"), verbose = T)
 crs(epu) <- crs
 
 run_sst_sews<- function(epu_name, season){
-
+  library(raster)
+  library(sf)
+  library(tidyverse)
 # masked seasonal raster stack to epu
 fname<- list.files(raw.dir, pattern = paste0(season,'_sst.nc'),full.names=TRUE)
 for (i in fname){
@@ -137,21 +59,24 @@ indices_test <- spatialwarnings::indictest(indices,
 plot(indices_test)+ ggplot2::ggtitle(paste(epu_name, season, "oisst"))
 
 # Save plot
-ggsave(filename = file.path("plots", paste0(epu_name,"_",season,"_OISST.png")))
+ggplot2::ggsave(filename = file.path("plots", paste0(epu_name,"_",season,"_OISST.png")))
 
 # Save data
+
 df <- tibble::enframe(indices) %>% 
-  mutate(name = as.numeric(df$name)) %>% 
-  mutate(Year = as.numeric(1981+(df$name))) %>% 
-  separate(value, c("skewness", "variance", "moran"),  ",") %>% 
-  separate(moran, c("before", "moran"),  "=") %>% 
-  mutate(moran = stringr::str_sub(df$moran, end=-2)) %>% 
-  separate(variance, c("before", "variance"), "=") %>% 
-  separate(skewness, c("a","b", "skewness"),  "=") %>% 
+  dplyr::mutate(name = as.numeric(df$name)) %>% 
+  dplyr::mutate(Year = as.numeric(1981+(df$name))) %>% 
+  tidyr::separate(value, c("skewness", "variance", "moran"),  ",") %>% 
+  tidyr::separate(moran, c("before", "moran"),  "=") %>% 
+  dplyr::mutate(moran = stringr::str_sub(df$moran, end=-2)) %>% 
+  tidyr::separate(variance, c("before", "variance"), "=") %>% 
+  tidyr::separate(skewness, c("a","b", "skewness"),  "=") %>% 
   dplyr::select(-a, -b, -before) %>% 
-  mutate(skewness = as.numeric(skewness), 
+  dplyr::mutate(skewness = as.numeric(skewness), 
          variance = as.numeric(variance), 
-         moran = as.numeric(moran))
+         moran = as.numeric(moran),
+         EPU = as.character(epu_name), 
+         season = as.character(season))
 
 write.csv(df, file=here::here("data", paste0(epu_name,"_",season,".csv")))
 }
