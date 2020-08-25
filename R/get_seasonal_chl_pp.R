d5457803c7e964cdcd539aba0ba06eb59c1bd7b0
kimberly-bastille <kimberly.bastille@gmail.com>
2020-08-25 08:33:58 -0400

    add data csvs

--- R/get_seasonal_chl_pp.R
+++ R/get_seasonal_chl_pp.R
@@ -5,8 +5,7 @@ library(ggplot2)
 library(ncdf4)
 library(reshape2)
 
-## data coming from here ftp://ftp.nefsc.noaa.gov/pub/satdata/HAAK/OCCCI/NETCDFS/
-
+## data coming from here ftp://ftp.nefsc.noaa.gov/pub/satdata/HAAK/OCCCI/NETCDFS
 
 
 
@@ -25,22 +24,27 @@ fname<- list.files(raw.dir, pattern='*.nc',full.names=TRUE)
 crs <- "+proj=longlat +lat_1=35 +lat_2=45 +lat_0=40
 +lon_0=-77 +x_0=0 +y_0=0 +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 
-
+chl.min<-0.0001
+chl.max<-100
 ## create and save raster stacks for each season
 winter <- list.files(raw.dir, pattern = "03-OCCCI")
 win_stack <- raster::stack(paste0(raw.dir,"/",winter))
+win_stack <- raster::clamp(win_stack, lower=chl.min, upper=chl.max, useValues=FALSE)
 raster::writeRaster(win_stack, filename=file.path(save.dir, "winter_chl.nc"), format = "CDF", overwrite = TRUE)
 
 spring <- list.files(raw.dir, pattern = "06-OCCCI")
 spr_stack <- raster::stack(paste0(raw.dir,"/",spring))
+spr_stack <- raster::clamp(spr_stack, lower=chl.min, upper=chl.max, useValues=FALSE)
 raster::writeRaster(spr_stack, filename=file.path(save.dir, "spring_chl.nc"), format = "CDF", overwrite = TRUE)
 
 summer <- list.files(raw.dir, pattern = "09-OCCCI")
 sum_stack <- raster::stack(paste0(raw.dir,"/",summer))
+sum_stack <- raster::clamp(sum_stack, lower=chl.min, upper=chl.max, useValues=FALSE)
 raster::writeRaster(sum_stack, filename=file.path(save.dir, "summer_chl.nc"), format = "CDF", overwrite = TRUE)
 
 fall <- list.files(raw.dir, pattern = "12-OCCCI")
 fal_stack <- raster::stack(paste0(raw.dir,"/",fall))
+fal_stack <- raster::clamp(fal_stack, lower=chl.min, upper=chl.max, useValues=FALSE)
 raster::writeRaster(fal_stack, filename=file.path(save.dir, "fall_chl.nc"), format = "CDF", overwrite = TRUE)
 
 
@@ -60,20 +64,26 @@ fname<- list.files(raw.dir, pattern='*.nc',full.names=TRUE)
 crs <- "+proj=longlat +lat_1=35 +lat_2=45 +lat_0=40
 +lon_0=-77 +x_0=0 +y_0=0 +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 
+pp.min <- 0.0001
+pp.max <- 20
 
 ## create and save raster stacks for each season
 winter <- list.files(raw.dir, pattern = "03-OCCCI")
 win_stack <- raster::stack(paste0(raw.dir,"/",winter))
+win_stack <- raster::clamp(win_stack, lower=pp.min, upper=pp.max, useValues=FALSE)
 raster::writeRaster(win_stack, filename=file.path(save.dir, "winter_pp.nc"), format = "CDF", overwrite = TRUE)
 
 spring <- list.files(raw.dir, pattern = "06-OCCCI")
 spr_stack <- raster::stack(paste0(raw.dir,"/",spring))
+spr_stack <- raster::clamp(spr_stack, lower=pp.min, upper=pp.max, useValues=FALSE)
 raster::writeRaster(spr_stack, filename=file.path(save.dir, "spring_pp.nc"), format = "CDF", overwrite = TRUE)
 
 summer <- list.files(raw.dir, pattern = "09-OCCCI")
 sum_stack <- raster::stack(paste0(raw.dir,"/",summer))
+sum_stack <- raster::clamp(sum_stack, lower=pp.min, upper=pp.max, useValues=FALSE)
 raster::writeRaster(sum_stack, filename=file.path(save.dir, "summer_pp.nc"), format = "CDF", overwrite = TRUE)
 
 fall <- list.files(raw.dir, pattern = "12-OCCCI")
 fal_stack <- raster::stack(paste0(raw.dir,"/",fall))
+fal_stack <- raster::clamp(fal_stack, lower=pp.min, upper=pp.max, useValues=FALSE)
 raster::writeRaster(fal_stack, filename=file.path(save.dir, "fall_pp.nc"), format = "CDF", overwrite = TRUE)
---
+++ R/run_sews.R
@@ -0,0 +1,134 @@
+library(raster)
+library(sf)
+library(tidyverse)
+library(dplyr)
+### Needed functions
+na_aware_ews <- function(mat, cg_subsize) { 
+  require(moments) # required packages
+  require(raster)
+  mat_coarse <- coarse_grain(mat, cg_subsize)
+  c(skewness = skewness(as.vector(mat_coarse), na.rm = TRUE), 
+    variance = var(as.vector(mat_coarse), na.rm = TRUE), 
+    moran    = Moran(raster(mat_coarse)))
+}
+
+randomize_matrix_no_na <- function(mat) {
+  mat[!is.na(mat)] <- sample(mat[!is.na(mat)])
+  return(mat)
+}
+
+
+gis.dir<-here::here("data-raw/gis")
+raw.dir<-here::here("data-raw")
+plot.dir<-here::here("plots")
+# Projection
+crs <- "+proj=longlat +lat_1=35 +lat_2=45 +lat_0=40 +lon_0=-77+x_0=0+y_0=0 +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
+
+# Bring in epu polygons
+epu <- rgdal::readOGR(file.path(gis.dir, "EPU_NOESTUARIES.shp"), verbose = T)
+crs(epu) <- crs
+
+epu_name<-"MAB"
+season<-"spring"
+indicator<-"sst"
+year_start<-1982
+
+run_sews<- function(epu_name, season, indicator, year_start){
+  library(raster)
+  library(sf)
+  library(tidyverse)
+  # masked seasonal raster stack to epu
+  fname<- list.files(raw.dir, pattern = paste0(season,'_',indicator,'.nc'),full.names=TRUE)
+  for (i in fname){
+    r_stack <- raster::stack(i)
+    masked<-raster::mask(r_stack, epu[epu$EPU == epu_name,])
+    masked<-raster::unstack(masked)
+  }
+  
+  # Run spatial ews analysis
+  library(spatialwarningsGis)
+  indices <- spatialwarnings::compute_indicator(masked, fun = na_aware_ews,
+                                                cg_subsize = 2) 
+  
+  #indices_test <- spatialwarnings::indictest(indices,
+  #                                           nulln = 999,
+  #                                           null_method = randomize_matrix_no_na)
+  
+  #plot(indices_test)+ ggplot2::ggtitle(paste(epu_name, season, indicator))
+  
+  # Save plot
+  #ggplot2::ggsave(filename = file.path("plots", paste0(epu_name,"_",season,"_",indicator,".png")))
+  
+  # Save data
+  library(dplyr)
+  df <- tibble::enframe(indices) %>% 
+    dplyr::mutate(Name = as.numeric(df$name)) %>% 
+    dplyr::mutate(Year = as.numeric(year_start + (df$name)-1)) %>% 
+    tidyr::separate(value, c("skewness", "variance", "moran"),  ",") %>% 
+    tidyr::separate(moran, c("before", "moran"),  "=") %>% 
+    dplyr::mutate(moran = stringr::str_sub(df$moran, end=-2)) %>% 
+    tidyr::separate(variance, c("before", "variance"), "=") %>% 
+    tidyr::separate(skewness, c("a","b", "skewness"),  "=") %>% 
+    dplyr::select(-a, -b, -before) %>% 
+    dplyr::mutate(skewness = as.numeric(skewness), 
+                  variance = as.numeric(variance), 
+                  moran = as.numeric(moran),
+                  EPU = as.character(epu_name), 
+                  season = as.character(season))
+  
+  write.csv(df, file=here::here("data", paste0(epu_name,"_",season,"_",indicator,".csv")))
+}
+
+
+
+
+### Run function for all EPUs and Seasons and indicators
+
+## CHLa
+run_sews("MAB", "winter", "chl", 1998)
+run_sews("MAB", "spring", "chl", 1998)
+run_sews("MAB", "summer", "chl", 1998)
+run_sews("MAB", "fall", "chl", 1998)
+
+run_sews("GB", "winter", "chl", 1998)
+run_sews("GB", "spring", "chl", 1998)
+run_sews("GB", "summer", "chl", 1998)
+run_sews("GB", "fall", "chl", 1998)
+
+run_sews("GOM", "winter", "chl", 1998)
+run_sews("GOM", "spring", "chl", 1998)
+run_sews("GOM", "summer", "chl", 1998)
+run_sews("GOM", "fall", "chl", 1998)
+
+## PP
+run_sews("MAB", "winter", "pp", 1998)
+run_sews("MAB", "spring", "pp", 1998)
+run_sews("MAB", "summer", "pp", 1998)
+run_sews("MAB", "fall", "pp", 1998)
+
+run_sews("GB", "winter", "pp", 1998)
+run_sews("GB", "spring", "pp", 1998)
+run_sews("GB", "summer", "pp", 1998)
+run_sews("GB", "fall", "pp", 1998)
+
+run_sews("GOM", "winter", "pp", 1998)
+run_sews("GOM", "spring", "pp", 1998)
+run_sews("GOM", "summer", "pp", 1998)
+run_sews("GOM", "fall", "pp", 1998)
+
+
+## SST
+run_sews("GOM", "winter", "sst", 1983)
+run_sews("GOM", "spring", "sst", 1983)
+run_sews("GOM", "summer", "sst", 1983)
+run_sews("GOM", "fall", "sst", 1983)
+
+run_sews("GB", "winter", "sst", 1983)
+run_sews("GB", "spring", "sst", 1983)
+run_sews("GB", "summer", "sst", 1983)
+run_sews("GB", "fall", "sst", 1983)
+
+run_sews("MAB", "winter", "sst", 1983)
+run_sews("MAB", "spring", "sst", 1983)
+run_sews("MAB", "summer", "sst", 1983)
+run_sews("MAB", "fall", "sst", 1983)
