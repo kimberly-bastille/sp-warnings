a5ded1217817a61664f12841071545c736ba5028
kimberly-bastille <kimberly.bastille@gmail.com>
2020-10-01 16:23:24 -0400

    update with ppd internal ccf

--- R/Comparative_analysis.R
+++ R/Comparative_analysis.R
@@ -2,6 +2,8 @@
 
 library(broom)
 library(dplyr)
+library(stringr)
+library(tidyverse)
 
 
 ######################################################################
@@ -22,6 +24,7 @@ pp <- ecodata::chl_pp %>%
     Year = as.numeric(Year))%>% 
   dplyr::group_by(Season, EPU, Year) %>% 
   dplyr::summarise(mean = mean(Value)) %>% 
+  dplyr::mutate(anom.mean = mean(mean) - mean) %>% 
   ungroup()
 
 
@@ -32,22 +35,44 @@ test<- pp %>%
   filter(EPU == epu_name, 
          Season == season_name)
 
-sst <- read.csv(file = paste0(data.dir, paste0(epu_name,"_",season_name,"_",indicator,".csv"))) %>% 
-  filter(Year > 1998)
+sews <- read.csv(file = paste0(data.dir, paste0(epu_name,"_",season_name,"_",indicator,".csv"))) %>% 
+  left_join(test) %>% 
+  dplyr::mutate(anom.var = mean(variance) - variance, 
+                anom.skew = mean(skewness) - skewness) 
 
-cc <- ccf(sst$variance, test$mean)
-print(cc)
-#p2<-plot(cc)
+ccf.values <- ccf( sews$anom.var, sews$anom.mean)
+ccf.df<-data.frame(lag=ccf.values$lag, CCF=ccf.values$acf) %>% 
+  dplyr::mutate(val = abs(CCF)) %>% 
+  dplyr::filter(val >= 0.42) %>% 
+  dplyr::select(!val) %>% 
+  dplyr::mutate("var~index")
+
+
+ccf.values2 <- ccf( sews$anom.skew, sews$anom.mean)
+ccf.df2<-data.frame(lag=ccf.values2$lag, CCF=ccf.values2$acf) %>% 
+  dplyr::mutate(val = abs(CCF)) %>% 
+  filter(val >= 0.42) %>% 
+  select(!val)%>% 
+  dplyr::mutate("skew~index") %>% 
+  rbind(ccf.df)
+
+sews2<- sews %>% 
+  tidyr::pivot_longer( cols = starts_with("anom"), 
+                       names_to = "Variable", 
+                       values_to = "Values")
   
-p1<- pp %>%
+p1<- sews2 %>%
   filter(EPU == epu_name,
          Season == season_name) %>%
   ggplot()+
-  geom_line(aes(x = Year, y = mean, color = "Mean pp"))+
-  geom_line(aes(x= indA$Year, y = indA$variance, color = "SST Variance"))+
-  ggtitle(paste("SST~PPD CCF", epu_name, season_name ))
+  geom_line(aes(x = Year, y = Values))+
+  geom_point(aes(x = Year, y = Values))+
+  facet_wrap(.~Variable, ncol = 1, scales = "free")+
+  annotation_custom(tableGrob(ccf.df), xmin=1998, xmax=2000, ymin=0, ymax=0.1)+
+  ggtitle(paste(indicator, epu_name, season_name ))
 p1
 
+head(ccf.df2)
 }
 
 ##### What does this mean??
@@ -72,7 +97,7 @@ ccf_test("GOM", "fall", "sst") # NO
 ###########################################################################
 
 ccf_test2<-function(epu_name, season_name, indicator){
-  zoo <- read.csv(here::here("data/zooplankton.csv")) %>% 
+zoo <- read.csv(here::here("data/zooplankton.csv")) %>% 
     filter(EPU == epu_name,
            season == season_name,
            zoo_name == "Large")
@@ -92,26 +117,27 @@ p1<- zoo1 %>%
   ggplot()+
   geom_line(aes(x =  year, y = anom1, color = "Zoo abundance"))+
   geom_line(aes(x= pp1$Year, y = pp1$variance, color = "PP Variance"))+
+  geom_line(aes(x= pp1$Year, y = pp1$skewness, color = "PP Skewness"))+
   ggtitle(paste("PPD~Zoo CCF", epu_name, season_name ))
 p1
 
 }
 
 ##### What does this mean??
-ccf_test2("MAB", "winter", "pp") # -4, 0, 6 
+ccf_test2("MAB", "winter", "pp") # NO 
 ccf_test2("MAB", "spring", "pp") # NO
-ccf_test2("MAB", "summer", "pp") # 9
-ccf_test2("MAB", "fall", "pp") # 9 
+ccf_test2("MAB", "summer", "pp") # 1 and 2 
+ccf_test2("MAB", "fall", "pp") # NO
 
 ccf_test2("GB", "winter", "pp") # NO
-ccf_test2("GB", "spring", "pp") # -1 
-ccf_test2("GB", "summer", "pp") # 5
-ccf_test2("GB", "fall", "pp")  #  NO
+ccf_test2("GB", "spring", "pp") # 0
+ccf_test2("GB", "summer", "pp") # -2
+ccf_test2("GB", "fall", "pp")  #  -3
 
 ccf_test2("GOM", "winter", "pp") # NO
-ccf_test2("GOM", "spring", "pp") # NO
-ccf_test2("GOM", "summer", "pp") # 8
-ccf_test2("GOM", "fall", "pp") # -6, 
+ccf_test2("GOM", "spring", "pp") # yes
+ccf_test2("GOM", "summer", "pp") # No
+ccf_test2("GOM", "fall", "pp") # -5 
 
 
 
